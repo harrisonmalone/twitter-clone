eaef5af491f3008bde4abc0c857c9936413d8390
Harrison Malone <harrison.malone2@gmail.com>
2020-11-02 12:33:10 +1100

    e2e tests for index page

--- spec/features/user_navigates_to_tweets_page_spec.rb
+++ spec/features/user_navigates_to_tweets_page_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature "on index page" do 
+  before do 
+    visit tweets_path
+  end
   scenario "user can see tweets" do 
-    expect(true).to eq(true)
+    expect(page).to have_css('.tweets')
+  end
+  scenario "the route is /tweets" do 
+    expect(page).to have_current_path(tweets_path)
+  end
+  scenario "user can click on button to take them to new tweet page" do 
+    click_on "New Tweet"
+    expect(page).to have_current_path(new_tweet_path)
   end
 end
